dce71eff59f552f967f84cd64b11fe61e13e50ca
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-06-28 17:04:59 +0800

    Added field to support the de centralised identity verification
    - added {identity: {type: "DNS", source: "abc.com"}}
    - put enum in type to ["DNS", "REGISTRY"]

--- schema/1.0/schema.test.ts
+++ schema/1.0/schema.test.ts
@@ -1,4 +1,4 @@
-import { omit } from "lodash";
+import { omit, merge } from "lodash";
 
 import { issueDocument, validateSchema } from "@govtechsg/open-attestation";
 
@@ -11,6 +11,37 @@ describe("schema/v1.0", () => {
     const valid = validateSchema(issuedDocument);
 
     expect(valid).toBe(true);
+  });
+
+  it("should be invalid if identity type is other than DNS and REGISTRY", () => {
+    const document = merge(sample, {
+      issuers: [
+        {
+          identity: {
+            type: "ABC",
+            source: "http:abc.com"
+          }
+        }
+      ]
+    });
+    expect(() => {
+      issueDocument(document, schema);
+    }).toThrow("Invalid document");
+  });
+
+  it("should be invalid if identity type is other than DNS and REGISTRY", () => {
+    const document = merge(sample, {
+      issuers: [
+        {
+          identity: {
+            type: "DNS",
+            source: "http:abc.com"
+          }
+        }
+      ]
+    });
+    const issuedDocument = issueDocument(document, schema);
+    expect(validateSchema(issuedDocument)).toBe(true);
   });
 
   it("should be valid with additonal key:value", () => {
